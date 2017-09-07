77ce3207b6843d591797389315ad61c0adcd8cb4
Tangshengbo <867850520tangbo@sina.com>
2017-09-07 16:23:47 +0800

    common refactor code

--- taotao-common/src/main/java/com/taotao/common/pojo/EasyUIDataGridResult.java
+++ taotao-common/src/main/java/com/taotao/common/pojo/EasyUIDataGridResult.java
@@ -6,33 +6,37 @@ import java.util.List;
  * EasyUI DataGrid的返回值类型
  * <p>Title: EasyUIDataGridResult</p>
  * <p>Description: </p>
- * <p>Company: www.itcast.com</p> 
- * @author	入云龙
- * @date	2015年8月13日上午11:56:29
+ * <p>Company: www.itcast.com</p>
+ *
  * @version 1.0
+ * @author 入云龙
+ * @date 2015年8月13日上午11:56:29
  */
 public class EasyUIDataGridResult {
 
-	private long total;
-	private List<?> rows;
-	
-	public EasyUIDataGridResult(long total, List<?> rows) {
-		this.total = total;
-		this.rows = rows;
-	}
-	
-	public long getTotal() {
-		return total;
-	}
-	public void setTotal(long total) {
-		this.total = total;
-	}
-	public List<?> getRows() {
-		return rows;
-	}
-	public void setRows(List<?> rows) {
-		this.rows = rows;
-	}
-	
-	
+    private long total;
+    private List<?> rows;
+
+    public EasyUIDataGridResult(long total, List<?> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<?> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<?> rows) {
+        this.rows = rows;
+    }
+
+
 }
